689684dd56845e1b346b492445e2c4bfe5f4c4de
Hideyuki Sawahata <jiospec@gmail.com>
2019-05-27 05:53:47 +0900

    code style arranged

--- editorLib/dataMange.js
+++ editorLib/dataMange.js
@@ -1,26 +1,26 @@
 const fs = require('fs');
 
-function dataSave(quill, option){
+function dataSave(quill, option) {
   //TODO: set interval of writing data is 15s ~ 20s.
   //TODO: write file when source from user.
   //TODO: filepath changed by date, but NOT change file path to loaded file.
   quill.on('text-change', (delta, oldDelta, source) => {
     if (source == 'api') {
-    console.log("An API call triggered this change.");
+      console.log("An API call triggered this change.");
     } else if (source == 'user') {
-    console.log("A user action triggered this change.");
-    content = JSON.stringify(quill.getContents(), null, 2);
+      console.log("A user action triggered this change.");
+      content = JSON.stringify(quill.getContents(), null, 2);
 
-    fs.writeFile('test.json', content, (err) => {
-      if (err) console.log(err);
-      else console.log('write successfull');
-    });
+      fs.writeFile('test.json', content, (err) => {
+        if (err) console.log(err);
+        else console.log('write successfull');
+      });
       //console.log(oldDelta);
       console.log(content);
     }
   });
 }
 
-module.exports={
+module.exports = {
   dataSave: dataSave
 };
