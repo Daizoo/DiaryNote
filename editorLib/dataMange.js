35e69c5d40af1a3a349dcc05dd0d93fbdb53fb4e
Hideyuki Sawahata <jiospec@gmail.com>
2019-05-29 10:53:02 +0900

    change fileSave system to interval from text-change

--- editorLib/dataMange.js
+++ editorLib/dataMange.js
@@ -1,22 +1,20 @@
 const fs = require('fs');
 
 function dataSave(quill, option) {
-  //TODO: set interval of writing data is 15s ~ 20s.
   //TODO: write file when source from user.
   //TODO: filepath changed by date, but NOT change file path to loaded file.
-  quill.on('text-change', (delta, oldDelta, source) => {
-    if (source == 'api') {
-      console.log("An API call triggered this change.");
-    } else if (source == 'user') {
-      console.log("A user action triggered this change.");
-      content = JSON.stringify(quill.getContents(), null, 2);
+  var ineterval = setInterval(() => {
+    var contents = JSON.stringify(quill.getContents(), null, 2);
+    fs.writeFile('testSave/test.json', contents, (err) => {
+      if (err) console.log(err)
+      else console.log('write successful.')
+    });
+  }, 20000);
 
-      fs.writeFile('test.json', content, (err) => {
-        if (err) console.log(err);
-        else console.log('write successfull');
-      });
-      //console.log(oldDelta);
-      console.log(content);
+  quill.on('text-change', (delta, oldDelta, source) => {
+    if (source == 'api'){
+      console.log('changed by API');
+      clearInterval(ineterval);
     }
   });
 }
